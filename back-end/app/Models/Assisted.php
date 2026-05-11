56adca5be32bc87d0fe62173f0b12602385e4e9d
JoaoGabrielMendonca <jgmr0308@gmail.com>
2026-05-11 00:17:09 -0300

    Assisted model ajustado pro banco oficial

--- back-end/app/Models/Assisted.php
+++ back-end/app/Models/Assisted.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Assisted extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'clients'; // aponta para a tabela existente no banco
+
+    protected $fillable = [
+        'creator_id',
+        'address_id',
+        'name',
+        'nickname',
+        'social_name',
+        'mother_name',
+        'father_name',
+        'kind_of_person',
+        'is_under_age',
+        'priority',
+        'cpf',
+        'cnpj',
+        'birth_date',
+        'rg',
+        'issuing_body',
+        'uf_issuing_body',
+        'marital_status',
+        'profession',
+        'education',
+        'monthly_income',
+        'nationality',
+        'naturalness',
+        'telephone',
+        'telephone2',
+        'email',
+        'email2',
+    ];
 }
