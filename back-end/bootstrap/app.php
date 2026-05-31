37171f1e5c50428aab85eacdaf81bae33cb88c1f
Antonio Jorge <antoniofilhojsa@gmail.com>
2026-05-31 13:55:19 -0300

    fix: add custom CORS middleware to fix preflight requests

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
+        $middleware->prepend([
+            \App\Http\Middleware\CorsMiddleware::class,
         ]);
 
         $middleware->validateCsrfTokens(except: [
