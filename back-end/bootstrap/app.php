ad5d9f5a49fa17cce91bee83e8cf6f288e8b093e
Antonio Jorge <antoniofilhojsa@gmail.com>
2026-05-31 14:03:47 -0300

    fix: corrigir CORS (remover middleware duplicado, habilitar credentials) e mover config:cache para railway.json

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -12,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend([
-            \App\Http\Middleware\CorsMiddleware::class,
-        ]);
-
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
